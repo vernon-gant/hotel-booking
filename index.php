2c071df6187837cb0e55fbb512d64c97270cf39f
Aleksandr Zakharov <aleksza.4119@gmail.com>
2022-10-16 09:33:33 +0200

    Continued designing home page(added 3 responsive cards)

--- index.php
+++ index.php
@@ -3,24 +3,72 @@ $title = "Motel X";
 require_once 'includes/header.php'
 ?>
 	<div class="container p-5">
-		<div class="row h-25" id="description">
-			<div class="col-10 col-md-12 row mx-auto">
+		<div class="card border-0 shadow-lg mx-auto mb-5 w-100">
+			<div class="row">
 				<div class="col-12 col-lg-6">
 					<img src="resources/img/main/bed.jpg"
 					     alt="beds"
-					     class="img-fluid rounded-3">
+					     class="card-img fit-cover w-100 h-100">
 				</div>
+				<div class="col-12 col-lg-6 my-auto">
+					<div class="card-body d-flex flex-column justify-content-between">
+						<h2 class="text-center card-title mb-4">Directly in the center of the city</h2>
+						<ul class="card-text">
+							<li class="mb-2">two&nbsp; freshly renovated&nbsp;studio apartments, each suitable for 4 guests:
+							             Kronberg &amp; Wiener Wald can accomodate up to 8 guests
+							</li>
+							<li class="mb-2">located in front of&nbsp;Schönbrunn Palace &amp; Vienna Zoo</li>
+							<li class="mb-2">2min walk from the city's&nbsp;U4 Metro Station "Hietzing"</li>
+							<li class="mb-2">10min from City Center via the U4 Metro line</li>
+							<li class="">supermarkt and several fine&nbsp;restaurants within a walking distance</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="card border-0 shadow-lg mx-auto mb-5 w-100">
+			<div class="row">
 				<div class="col-12 col-lg-6">
-					<h2 class="text-center">Directly in the center of the city</h2>
-					<ul class="list-group-horizontal">
-						<li class="my-auto">two&nbsp; freshly renovated&nbsp;studio apartments, each suitable for 4 guests: Kronberg
-						    &amp; Wiener Wald can accomodate up to 8 guests
-						</li>
-						<li class="my-auto">located in front of&nbsp;Schönbrunn Palace &amp; Vienna Zoo</li>
-						<li class="my-auto">2min walk from the city's&nbsp;U4 Metro Station "Hietzing"</li>
-						<li class="my-auto">10min from City Center via the U4 Metro line</li>
-						<li class="my-auto">supermarkt and several fine&nbsp;restaurants within a walking distance</li>
-					</ul>
+					<img src="resources/img/main/lounge_bar.jpg"
+					     alt="beds"
+					     class="card-img fit-cover w-100 h-100">
+				</div>
+				<div class="col-12 col-lg-6 my-auto">
+					<div class="card-body d-flex flex-column justify-content-between">
+						<h2 class="text-center card-title mb-4">Directly in the center of the city</h2>
+						<ul class="card-text">
+							<li class="mb-2">two&nbsp; freshly renovated&nbsp;studio apartments, each suitable for 4 guests:
+							                 Kronberg &amp; Wiener Wald can accomodate up to 8 guests
+							</li>
+							<li class="mb-2">located in front of&nbsp;Schönbrunn Palace &amp; Vienna Zoo</li>
+							<li class="mb-2">2min walk from the city's&nbsp;U4 Metro Station "Hietzing"</li>
+							<li class="mb-2">10min from City Center via the U4 Metro line</li>
+							<li class="">supermarkt and several fine&nbsp;restaurants within a walking distance</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="card border-0 shadow-lg mx-auto mb-5 w-100">
+			<div class="row">
+				<div class="col-12 col-lg-6">
+					<img src="resources/img/main/terasse.jpg"
+					     alt="beds"
+					     class="card-img fit-cover w-100 h-100">
+				</div>
+				<div class="col-12 col-lg-6 d-flex flex-column">
+					<div class="card-body d-flex flex-column flex-grow-1">
+						<h2 class="text-center card-title mb-4">Directly in the center of the city</h2>
+						<ul class="card-text flex-grow-1 d-flex flex-column justify-content-around">
+							<li class="mb-2 lh-lg">two&nbsp; freshly renovated&nbsp;studio apartments, each suitable for 4 guests:
+							                 Kronberg &amp; Wiener Wald can accomodate up to 8 guests
+							</li>
+							<li class="mb-2 lh-lg">located in front of&nbsp;Schönbrunn Palace &amp; Vienna Zoo</li>
+							<li class="mb-2 lh-lg">2min walk from the city's&nbsp;U4 Metro Station "Hietzing"</li>
+							<li class="mb-2 lh-lg">10min from City Center via the U4 Metro line</li>
+							<li class="">supermarkt and several fine&nbsp;restaurants within a walking distance</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
