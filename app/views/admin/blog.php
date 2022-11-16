d938ab88ce95401b1178b5180fcd8810dabe46d0
Aleksandr Zakharov <aleksza.4119@gmail.com>
2022-11-16 21:24:03 +0100

    Fixed admin sidebar with blog.

--- app/views/admin/blog.php
+++ app/views/admin/blog.php
@@ -3,18 +3,9 @@ require_once APPROOT . '/views/includes/head.php';
 require_once APPROOT . '/views/includes/admin_sidebar.php';
 ?>
 <div class="d-flex flex-column h-100">
-    <div class="p-5 bg-light rounded-3 d-flex flex-grow-1">
-        <div class="container-fluid py-5 d-flex flex-column align-items-center">
-            <div class="container-fluid">
-                <div class="col">
-                    <h1>Posts</h1>
-                </div>
-                <div class="col">
-                    <a href="<?php echo URL_ROOT; ?>/posts/add" class="btn btn-primary pull-right">
-                        <i class="fa fa-pencil"></i> Add Post
-                    </a>
-                </div>
-            </div>
+    <div class="py-5 px-2 bg-light rounded-3 d-flex flex-grow-1">
+        <div class="container-fluid d-flex flex-column align-items-center justify-content-between">
+            <h1>Posts</h1>
             <div class="row mx-auto">
 				<?php if (isset($data['posts'])) : ?>
 					<?php foreach ($data['posts'] as $post) : ?>
@@ -34,9 +25,12 @@ require_once APPROOT . '/views/includes/admin_sidebar.php';
                         </div>
 					<?php endforeach; ?>
 				<?php else: ?>
-                    <h2>No posts yet</h2>
+                    <h2>No posts yet...</h2>
 				<?php endif ?>
             </div>
+            <a href="<?php echo URL_ROOT; ?>/posts/add" class="btn btn-primary pull-right">
+                <i class="fa fa-pencil"></i> Add Post
+            </a>
         </div>
     </div>
 </div>
--- app/views/includes/admin_sidebar.php
+++ app/views/includes/admin_sidebar.php
@@ -1,42 +1,42 @@
 <body class="d-flex flex-column vh-100 overflow-hidden">
-    <div class="container-fluid d-flex flex-sm-row flex-column flex-grow-1 pb-3 overflow-auto">
-        <div class="row flex-grow-sm-1 flex-grow-0 p-3">
+    <div class="container-fluid d-flex flex-sm-row flex-md-column flex-grow-1 pb-3 overflow-auto">
+        <div class="row flex-grow-sm-1 flex-grow-1 p-3">
             <aside class="col-sm-4 col-md-3 col-lg-2 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3">
-            <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
-                <ul class="mt-3 nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate align-items-center">
-                    <li class="nav-item my-3">
-                        <a href="<?php echo URL_ROOT . '/admin/index'?>" class="nav-link px-2 text-truncate">
-                            <i class="fa-solid fa-house fs-5"></i>
-                            <span class="d-none d-sm-inline">Home</span>
-                        </a>
-                    </li>
-                    <li class="nav-item mb-3">
-                        <a href="<?php echo URL_ROOT . '/admin/users'?>" class="nav-link px-2 text-truncate">
-                            <i class="fa-solid fa-user fs-5"></i>
-                            <span class="d-none d-sm-inline">Users</span>
-                        </a>
-                    </li>
-                    <li class="nav-item mb-3">
-                        <a href="<?php echo URL_ROOT . '/admin/bookings'?>" class="nav-link px-2 text-truncate">
-                            <i class="fa-solid fa-clock fs-5"></i>
-                            <span class="d-none d-sm-inline">Bookings</span>
-                        </a>
-                    </li>
-                    <li class="nav-item mb-3">
-                        <a href="<?php echo URL_ROOT . '/admin/blog'?>" class="nav-link px-2 text-truncate">
-                            <i class="fa-solid fa-blog fs-5"></i>
-                            <span class="d-none d-sm-inline">Blog</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="<?php echo URL_ROOT . '/admin/logout'?>" class="nav-link px-2 text-truncate">
-                            <i class="fa-solid fa-egg fs-5"></i>
-                            <span class="d-none d-sm-inline">Logout</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </aside>
+                <div class="bg-light border rounded-3 px-3 px-sm-0 p-sm-2 p-md-1 h-100 sticky-top">
+                    <ul class="my-3 nav nav-pills flex-sm-column flex-row justify-content-between text-truncate align-items-center">
+                        <li class="nav-item my-0 my-sm-3">
+                            <a href="<?php echo URL_ROOT . '/admin/index'?>" class="nav-link px-2 text-truncate">
+                                <i class="fa-solid fa-house fs-5"></i>
+                                <span class="d-none d-sm-inline">Home</span>
+                            </a>
+                        </li>
+                        <li class="nav-item my-0 my-sm-3">
+                            <a href="<?php echo URL_ROOT . '/admin/users'?>" class="nav-link px-2 text-truncate">
+                                <i class="fa-solid fa-user fs-5"></i>
+                                <span class="d-none d-sm-inline">Users</span>
+                            </a>
+                        </li>
+                        <li class="nav-item my-0 my-sm-3">
+                            <a href="<?php echo URL_ROOT . '/admin/bookings'?>" class="nav-link px-2 text-truncate">
+                                <i class="fa-solid fa-clock fs-5"></i>
+                                <span class="d-none d-sm-inline">Bookings</span>
+                            </a>
+                        </li>
+                        <li class="nav-item my-0 my-sm-3">
+                            <a href="<?php echo URL_ROOT . '/admin/blog'?>" class="nav-link px-2 text-truncate">
+                                <i class="fa-solid fa-blog fs-5"></i>
+                                <span class="d-none d-sm-inline">Blog</span>
+                            </a>
+                        </li>
+                        <li class="nav-item my-0 my-sm-3">
+                            <a href="<?php echo URL_ROOT . '/admin/logout'?>" class="nav-link px-2 text-truncate">
+                                <i class="fa-solid fa-egg fs-5"></i>
+                                <span class="d-none d-sm-inline">Logout</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
             <main class="col overflow-auto h-100">
 
 
