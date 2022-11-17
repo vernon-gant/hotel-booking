eacce3994732b7742a7077ae890c66cc11930102
Aleksandr Zakharov <aleksza.4119@gmail.com>
2022-11-17 15:45:13 +0100

    Designed single post on blog page + overall page layout, completed template for each post and added method in Post Model for retrieving ALL posts from the db, createPost method and getAdminPosts method

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,12 +4,42 @@ class Post {
 
 	private Database $db;
 
-	public function __construct() {
-		$this->db = new Database();
+	private RandomStringGenerator $generator;
+
+	/**
+	 * @return RandomStringGenerator
+	 */
+	public function getGenerator(): RandomStringGenerator {
+		return $this->generator;
 	}
 
-	public function createPost(array $data) {
+	public static int $idLength = 10;
 
+	public function __construct() {
+		$this->db = new Database();
+		$this->generator = new RandomStringGenerator(implode(range('a', 'z')));
+	}
+
+	public function createPost(array $data): bool {
+		$this->db->query("INSERT INTO 
+    						  posts  (id,user_email, title, body, img)
+							  VALUES (?,?,?,?,?);", ...$data);
+		return $this->db->affectedRows() > 0;
+	}
+
+	public function getAdminPosts(): ?array {
+		$this->db->query("SELECT id, title, body, img, posts.created_at, first_name, last_name 
+							  FROM posts join users u on u.email = posts.user_email 
+							  WHERE user_email = ?", $_SESSION['admin_email']);
+		if ($this->db->rowCount() > 0) return $this->db->resultSet();
+		else return null;
+	}
+
+	public function fetchAllPosts(): ?array {
+		$this->db->query("SELECT id, title, body, img, posts.created_at, first_name, last_name 
+						      FROM posts join users u on u.email = posts.user_email");
+		if ($this->db->rowCount() > 0) return $this->db->resultSet();
+		else return null;
 	}
 
 }
--- app/views/pages/blog.php
+++ app/views/pages/blog.php
@@ -2,7 +2,32 @@
 require_once APPROOT . '/views/includes/head.php';
 require_once APPROOT . '/views/includes/navbar.php'
 ?>
-	<div class="bg-dark">
-<!--		TODO start designing single blog entry element-->
+	<div class="container p-5">
+        <?php if (isset($data['posts'])) : ?>
+
+                <?php foreach ($data['posts'] as $index => $post): ?>
+
+                    <?php if ($index % 3 == 0) echo "<div class='row mx-auto mb-5'>"?>
+
+                        <div class="col-12 col-md-4 mx-auto d-flex align-items-stretch">
+                            <div class="card">
+                                <div class="card-block d-flex flex-column justify-content-between h-100">
+					    			<?php if (isset($post['img'])) echo '<img class="card-img-top" alt="post photo" src="' . mapImagePathToPhoto($post["img"]) .'">'?>
+                                    <h4 class="card-title text-center my-2 <?php if (!isset($post['img'])) echo "p-4" ?>"><?php echo $post['title']?></h4>
+                                    <h6 class="card-subtitle text-muted text-center mt-2">Written by <?php echo $post['first_name'] . " " . $post['last_name']?></h6>
+                                    <p class="card-text p-3 my-2 text-center"><?php echo substr($post['body'],0,60) . "..."?></p>
+                                    <p class="card-footer mb-0 text-center"><?php echo date_format(date_create($post['created_at']),"l jS \of F Y")?></p>
+                                </div>
+                            </div>
+                        </div>
+
+				    <?php if ($index % 2 == 0 and $index != 0) echo "</div>"?>
+
+                <?php endforeach?>
+        <?php else: ?>
+        <div class="d-flex flex-column flex-grow-1 w-100 justify-content-center">
+            <h1>No posts yet...</h1>
+        </div>
+        <?php endif;?>
 	</div>
 <?php require_once APPROOT . '/views/includes/footer.php' ?>
